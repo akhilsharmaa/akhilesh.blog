85179cebaa1de12eb3e2f872da50d38b5d241fc0
Akhilesh <akhilsharma3332@gmail.com>
2025-03-12 14:09:58 +0530

    [Open source] PR section created, - pull_request object created. - tags etc fetching at the home page.

--- src/content.config.ts
+++ src/content.config.ts
@@ -34,4 +34,35 @@ const project = defineCollection({
 	}),
 });
 
-export const collections = { blog, project };
+
+const pull_request = defineCollection({
+	// Load Markdown and MDX files in the `src/content/project/` directory.
+	loader: glob({ base: './src/content/pull_request', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		orgName: z.string().optional(),
+		orgLink: z.string().optional(), 
+		orgDescription: z.string().optional(), 
+		repoName: z.string().optional(), 
+		repoLink: z.string().optional(), 
+		orgLabels: z.array(z.string()).optional(),
+		
+		prTitle: z.string().optional(),
+		prNumber: z.string().optional(),
+		prLink: z.string().optional(),
+		prStatus: z.string().optional(),
+		prDescription: z.string().optional(),
+		prDate: z.coerce.date().optional(),
+		prLabels: z.array(z.string()).optional(),
+		
+		issueTitle: z.string().optional(),
+		issueNumber: z.string().optional(),
+		issueLink: z.coerce.date().optional(),
+		issueDescription: z.string().optional(),
+		issueStatus: z.string().optional(),
+		issueDate: z.coerce.date().optional().optional(),
+		issueLabels: z.array(z.string()).optional(),
+	}),
+});
+
+export const collections = { blog, project, pull_request};
